8f94e3d7264efcd5353ab6b07516ce78c0c0cdc0
satyabuddharajuvenkata <buddharaju.varma@icloud.com>
2025-02-21 16:44:13 -0600

    commit

--- RecordForms/force-app/main/default/lwc/recordFormDemo/recordFormDemo.js
+++ RecordForms/force-app/main/default/lwc/recordFormDemo/recordFormDemo.js
@@ -1,3 +1,12 @@
-import { LightningElement } from 'lwc';
-
-export default class RecordFormDemo extends LightningElement {}
+import { LightningElement, api } from 'lwc';
+import NAME_FIELD from '@salesforce/schema/Account.Name'; 
+import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry'; 
+import RATING_FIELD from '@salesforce/schema/Account.Rating'; 
+import REVENUE_FILED from '@salesforce/schema/Account.AnnualRevenue'; 
+export default class RecordFormDemo extends LightningElement {
+    //Expose fields to make it available in the template 
+    fieldList = [NAME_FIELD, INDUSTRY_FIELD, RATING_FIELD, REVENUE_FILED]; 
+    
+    @api recordId; 
+    @api objectApiName; 
+} 
