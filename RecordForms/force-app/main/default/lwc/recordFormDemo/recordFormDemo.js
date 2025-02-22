930825ade7e6b8b797e0423b171b8d9785835f92
satyabuddharajuvenkata <buddharaju.varma@icloud.com>
2025-02-21 19:00:31 -0600

    changesDone

--- RecordForms/force-app/main/default/lwc/recordFormDemo/recordFormDemo.js
+++ RecordForms/force-app/main/default/lwc/recordFormDemo/recordFormDemo.js
@@ -1,12 +1,35 @@
 import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { NavigationMixin } from 'lightning/navigation';
 import NAME_FIELD from '@salesforce/schema/Account.Name'; 
 import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry'; 
 import RATING_FIELD from '@salesforce/schema/Account.Rating'; 
 import REVENUE_FILED from '@salesforce/schema/Account.AnnualRevenue'; 
-export default class RecordFormDemo extends LightningElement {
+export default class RecordFormDemo extends NavigationMixin(LightningElement) {
     //Expose fields to make it available in the template 
     fieldList = [NAME_FIELD, INDUSTRY_FIELD, RATING_FIELD, REVENUE_FILED]; 
     
     @api recordId; 
     @api objectApiName; 
+
+    showToast() {
+        const event = new ShowToastEvent({
+            title: 'Success',
+            message: 'Record Updated Successfully',
+            variant : 'success'
+        });
+        this.dispatchEvent(event);
+    }
+
+    navigateToRecordPage(event) {
+        let PageRef = {
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: event.detail.id,
+                objectApiName: this.objectApiName,
+                actionName: 'view'
+            }
+        };
+        this[NavigationMixin.Navigate](PageRef);
+    }
 } 
