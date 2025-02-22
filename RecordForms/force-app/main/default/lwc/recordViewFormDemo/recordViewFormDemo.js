55ec68a0b1cef8a583dce8195e244e0dafeca964
satyabuddharajuvenkata <buddharaju.varma@icloud.com>
2025-02-21 21:56:38 -0600

    RecordViewForm

---
+++ RecordForms/force-app/main/default/lwc/recordViewFormDemo/recordViewFormDemo.js
@@ -0,0 +1,16 @@
+import { LightningElement, api } from 'lwc';
+import NAME_FIELD from '@salesforce/schema/Account.Name'; 
+import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry'; 
+import RATING_FIELD from '@salesforce/schema/Account.Rating'; 
+import REVENUE_FILED from '@salesforce/schema/Account.AnnualRevenue'; 
+export default class RecordViewFormDemo extends LightningElement {
+    fieldObject = {
+        Name : NAME_FIELD,
+        Industry : INDUSTRY_FIELD,
+        Rating : RATING_FIELD,
+        Revenue : REVENUE_FILED
+    }
+
+    @api recordId;
+    @api objectApiName;
+}
